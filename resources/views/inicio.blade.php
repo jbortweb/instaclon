2cfb4083c03c8205051873e2107ce69de826a4ba
Jbortweb <jbortweb@gmail.com>
2023-10-30 11:35:52 +0100

    componente ListarPost

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,26 +6,5 @@ Inicio
 
 @section('contenido')
 
-@if ($posts->count())
-
-<div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-  @foreach ($posts as $post)
-  <div>
-  <p class="text-gray-500 font-bold uppercase text-sm text-center my-4">{{$post->titulo}}</p>
-    <a href="{{route('posts.show', ['post'=>$post, 'user'=>$post->user])}}">
-      <img src="{{ asset('uploads') . '/' .$post->imagen}}" alt="Imagen del post {{$post->titulo}}">
-    </a>
-    <p class="text-gray-600 text-sm text-center my-4"><span class="font-bold">Autor: </span>{{$post->user->username}}</p>
-  </div>
-  @endforeach
-</div>
-
-<div class="my-10">
-  {{$posts->links()}}
-</div>
-  
-@else
-  <p class="text-center">No se muestran publicaciones, debes de seguir a alguien para poder mostrarlas</p>
-@endif
-
+  <x-listar-post :posts='$posts'/>
 @endsection
